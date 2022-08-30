47ddeb75e5a67a67a15493a496710f60489da215
Mikhail Malafeew <malafeew2016@gmail.com>
2022-08-30 19:29:42 +0300

    Ex_001

---
+++ Ex_001/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 4
+bool isWork = true;
+string decision = String.Empty;
+int count = 0;
+while(isWork)
+{
+    Console.Write("Введите число или напишите 'Exit': ");
+    decision = Console.ReadLine();
+    if(decision.ToLower() == "exit")
+    {
+        isWork = false;
+    }
+    else
+    {
+        if(Convert.ToInt32(decision) > 0)
+        {
+            count++;
+        }
+    }
+}
+Console.WriteLine($"Количетсво чисел, больше 0 = {count}");
